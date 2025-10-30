84ad5a140aab946d36be1db246f5db9e3f9add39
Josiah Brown <josiahd7.brown@gmail.com>
2025-10-30 19:23:03 -0400

    Added a tower that appears/disappears when you click the black button

--- public/components/toggle-target-on-click.js
+++ public/components/toggle-target-on-click.js
@@ -1,14 +1,19 @@
 AFRAME.registerComponent('toggle-target-on-click', {
-schema: { target: { type: 'selector' } },
+  schema: { target: { type: 'selector' } },
 
-init: function () {
+  init: function () {
     const el = this.el;
-    const targetEl = this.data.target;
 
-    el.addEventListener('click', function () {
-    const isVisible = targetEl.getAttribute('visible');
-    targetEl.setAttribute('visible', !isVisible);
+    el.addEventListener('click', () => {
+      const targetEl = this.data.target;
+      if (!targetEl) {
+        console.warn('[toggle-target-on-click] No target found for', el);
+        return;
+      }
+
+      const isVisible = targetEl.getAttribute('visible');
+      targetEl.setAttribute('visible', !isVisible);
+      console.log('Toggled visibility. Was:', isVisible, 'Now:', !isVisible);
     });
-}
+  }
 });
-
